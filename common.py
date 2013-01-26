a2afee496462eeb9773a15029e7d74df52a6b90c
Victor Vasiliev <vasilvv@gmail.com>
2013-01-26 14:16:23 -0500

    Initial commit.
    This includes mrlist commnad, as well as some common bits for all
    the future command-line tools.

---
+++ common.py
@@ -0,0 +1,110 @@
+#
+## mrtools shared code
+#
+
+import datetime, colorama, argparse, sys
+import pymoira
+
+mrtools_version = '0.1'
+
+def get_version(program):
+	"""Return the displayable version of the program"""
+
+	return "%s from mrtools %s" % (program, mrtools_version)
+
+def section_header(text):
+	"""Output a section header"""
+
+	print "---- %s ----" % text
+
+def emph_text(text):
+	"""Make a text bold, unless the text formatting is disabled."""
+
+	if not args.no_color:
+		return colorama.Style.BRIGHT + text + colorama.Style.RESET_ALL
+	else:
+		return text
+
+def color_text(text, color_name):
+	"""Make the text of a specific color, unless the text formatting is disabled."""
+
+	if not args.no_color:
+		return colorama.Fore.__dict__[color_name.upper()] + text + colorama.Style.RESET_ALL
+	else:
+		return text
+
+def show_fields(*fields):
+	"""Output the {field label -> field value} dictionary. Does the alignment
+	and formats certain specific types of values."""
+
+	fields = filter( lambda x: x, fields )
+	target_len = max( len(name) for name, value in fields ) + 2
+	for name, value in fields:
+		line = name + ':' + " " * (target_len - len(name))
+		if type(value) == bool:
+			line += color_text("Yes", 'green') if value else color_text("No", 'red')
+		else:
+			line += str(value)
+		print line
+
+def plural(num, one, many):
+	"""Convenience function for displaying a numeric value, where the attached noun
+	may be both in singular and in plural form."""
+
+	return "%i %s" % (num, one if num == 1 else many)
+
+def last_modified_date(when):
+	"""Formats the last modification date in a human-readable format."""
+
+	# FIXME: this should use the Moira server timezone
+	delta = datetime.datetime.now() - when
+	if delta.days > 0:
+		if delta.days > 365:
+			return "%.2f years" % (delta.days / 365.25)
+		else:
+			return plural(delta.days, "day", "days")
+	else:
+		if delta.seconds > 3600:
+			hours = delta.seconds / 3600
+			minutes = (delta.seconds - hours * 3600) / 60
+			return plural(hours, "hour", "hours") + ' ' + plural(minutes, "minute", "minutes")
+		elif delta.seconds > 60:
+			return plural(delta.seconds / 60, "minute", "minutes")
+		else:
+			return plural(delta.seconds, "second", "seconds")
+
+# ------------------------------------------------------------------------------
+
+def init(name, description, subparsers_generator):
+	"""Parses the command-line arguments, initializes the client and then returns both the client
+	and the arguments."""
+
+	global client, args
+
+	argparser = argparse.ArgumentParser(description = description)
+	argparser.add_argument('--version', action = 'version', version = get_version(name), help = 'Output program version and quit')
+	argparser.add_argument('--no-color', action = 'store_true', help = 'Disable the color output')
+	argparser.add_argument('-a', '--anonymous', action = 'store_true', help = 'Do not log into Moira')
+	subparsers = argparser.add_subparsers()
+	
+	if subparsers_generator:
+		subparsers_generator(argparser, subparsers)
+	
+	args = argparser.parse_args()
+	
+	try:
+		client = pymoira.Client()
+		if not args.anonymous:
+			client.authenticate(name)
+
+		return client, args
+	except pymoira.BaseError as err:
+		sys.stderr.write( "%s\n" % err )
+
+def main():
+	"""Executes the relevant subcommand."""
+
+	try:
+		args.handler()
+	except pymoira.BaseError as err:
+		sys.stderr.write( "%s\n" % err )
---
+++ mrlist.py
@@ -0,0 +1,187 @@
+#!/usr/bin/python
+
+#
+## mrlist
+##
+## This program provides an interface to read and manipulate Moira lists.
+#
+
+import common
+import re, argparse
+
+import pymoira
+from pymoira import List, ListMember, ListTracer
+
+def resolve_member(name, default_to_string):
+	"""Resolve the list member name."""
+	
+	member = ListMember.resolveName(client, name)
+	if member:
+		return member
+	if re.match( "^[a-z0-9_]{3,8}.(root|extra|dbadmin)$", name ):
+		return ListMember( client, MoiraListMember.Kerberos, name + '@ATHENA.MIT.EDU' )
+	
+	if default_to_string:
+		return ListMember( client, MoiraListMember.String, name )
+	else:
+		return None
+
+def output_member_list(members, hook = None):
+	"""Output the list of list members."""
+	
+	members = list(members)
+	members.sort()
+	type_headers = (
+		('USER', 'Users'),
+		('KERBEROS', 'Kerberos principals'),
+		('LIST', 'Sublists'),
+		('STRING', 'String entries (includes non-MIT emails)'),
+		('MACHINE', 'Machines'),
+	)
+	for list_type, header in type_headers:
+		displayed = [member for member in members if member.mtype == list_type]
+		if displayed:
+			common.section_header(header)
+			for member in displayed:
+				if hasattr(member, 'tag') and member.tag:
+					print "* %s [%s]" % (member.name, member.tag)
+				else:
+					print "* %s" % member.name
+				if hook:
+					hook(member)
+			print	# FIXME: this should not be done in case of last entry
+
+def show_info():
+	"""Handle 'mrlist info'."""
+
+	mlist = List(common.client, args.list)
+	mlist.loadInfo()
+
+	common.section_header("Information about list %s" % common.emph_text(mlist.name) )
+	common.show_fields(
+		('Description', mlist.description),
+		('Active', mlist.active),
+		('Public', mlist.public),
+		('Visible', not mlist.hidden),
+		('Mailing list', mlist.is_mailing),
+		('AFS group', "GID #%s" % mlist.gid if mlist.is_afsgroup else mlist.is_afsgroup),
+		('Unix group', mlist.is_nfsgroup) if mlist.is_afsgroup else None,
+		('Mailman list', "On server %s" % mlist.mailman_server if mlist.is_mailman_list else mlist.is_mailman_list),
+		('Owner', "%s %s" % (mlist.owner_type.lower(), mlist.owner_name) ),
+		('Membership ACL', "%s %s" % (mlist.memacl_type.lower(), mlist.memacl_name) if mlist.memacl_type != 'NONE' else 'None' ),
+		('Last modified', "%s ago by %s using %s" % (common.last_modified_date(mlist.lastmod_datetime), mlist.lastmod_by, mlist.lastmod_with)),
+	)
+
+def show_inverse():
+	"""Handle 'mrlist inverse'."""
+
+	member = resolve_member(args.member, False)
+	if not member:
+		print "Impossible to determine the type of a member. Please specify it explicitly by prefixing the member with type, seperated by colon, like this: string:test@example.com"
+		return
+
+	memberships = list( member.getMemberships(recursive = args.recursive) )
+	memberships.sort()
+	common.section_header( "Memberships of %s" % common.emph_text( str(member) ) )
+	for mlist in memberships:
+		print "* %s" % mlist.name
+
+def show_members():
+	"""Handle 'mrlist members'."""
+
+	mlist = List(common.client, args.list)
+	members = mlist.getExplicitMembers(tags = args.verbose)
+	output_member_list(members)
+
+def expand_list():
+	"""Handle 'mrlist expand'."""
+	
+	mlist = List(common.client, args.list)
+	if args.server_side:
+		members = mlist.getAllMembers(server_side = True, include_lists = args.include_lists)
+		if args.trace:
+			print "Server-side tracing is not supported"
+			return
+	else:
+		if args.trace:
+			tracer = ListTracer(mlist)
+			members = tracer.members
+			inaccessible = tracer.inaccessible
+			lists = tracer.lists
+
+			# List tracer includes lists by default
+			if not args.include_lists:
+					lists = [mlist for mlist in lists if mlist.type != ListMember.List]
+		else:
+			members, inaccessible, lists = mlist.getAllMembers(include_lists = args.include_lists)
+		
+		print "List %s contains %i members, found by expanding %i nested lists (access denied to %i)" % (mlist.name, len(members), len(lists), len(inaccessible))
+	
+	if args.trace:
+		def show_trace(member):
+			pathways = tracer.trace(member)
+			if len(pathways) > 3 and not args.verbose:
+				print "   [%i inclusion pathways]" % len(pathways)
+			else:
+				for pathway in pathways:
+					print "   - " + " -> ".join( pathway + (member.name,) )
+		
+		output_member_list(members, show_trace)
+	else:
+		output_member_list(members)
+
+def add_member():
+	"""Handle 'mrlist add'."""
+
+	mlist = List(client.client, args.list)
+	member = resolve_member(args.member, True)
+	mlist.addMember(member)
+	print "Added %s to list %s" % (common.emph_text( str(member) ), common.emph_text( mlist.name ))
+
+def remove_member():
+	"""Handle 'mrlist remove'."""
+
+	mlist = List(client.client, args.list)
+	member = resolve_member(args.member, True)
+	mlist.removeMember(member)
+	print "Removed %s from list %s" % (common.emph_text( str(member) ), common.emph_text( mlist.name ))
+
+def setup_subcommands(argparser, subparsers):
+	"""Sets up all the subcommands."""
+
+	parser_add = subparsers.add_parser('add', help = 'Add a member to the list')
+	parser_add.add_argument('list', help = 'The list to which the member will be added')
+	parser_add.add_argument('member', help = 'The member to add to the list')
+
+	parser_expand = subparsers.add_parser('expand', help = 'Recursively expands all sublists in the specified list')
+	parser_expand.add_argument('list', help = 'The name of the list to show information about')
+	parser_expand.add_argument('-l', '--include-lists', action = 'store_true', help = 'Display all the nested lists')
+	parser_expand.add_argument('-s', '--server-side', action = 'store_true', help = argparse.SUPPRESS)	# for debug purposes
+	parser_expand.add_argument('-t', '--trace', action = 'store_true', help = 'Show the paths through which the given user is included')
+	parser_expand.add_argument('-v', '--verbose', action = 'store_true', help = 'Do not hide the inclusion pathways even if there are many of them')
+
+	parser_info = subparsers.add_parser('info', help = 'Provide the information about the list')
+	parser_info.add_argument('list', help = 'User, Kerberos principal or ')
+
+	parser_inverse = subparsers.add_parser('inverse', help = 'Show the lists into which certain member is included')
+	parser_inverse.add_argument('member', help = 'The name of the list to show information about')
+	parser_inverse.add_argument('-r', '--recursive', action = 'store_true', help = 'Show the lists into which a member is included through other lists')
+
+	parser_members = subparsers.add_parser('members', aliases=('mem',), help = 'Shows the members explicitly included into the list')
+	parser_members.add_argument('list', help = 'The name of the list to show information about')
+	parser_members.add_argument('-v', '--verbose', action = 'store_true', help = 'Output explicitly the members of the list')
+
+	parser_remove = subparsers.add_parser('remove', help = 'Remove a member from the list')
+	parser_remove.add_argument('list', help = 'The list from which the member will be removed')
+	parser_remove.add_argument('member', help = 'The member to remove from the list')
+
+	parser_add.set_defaults(handler = add_member)
+	parser_expand.set_defaults(handler = expand_list)
+	parser_info.set_defaults(handler = show_info)
+	parser_inverse.set_defaults(handler = show_inverse)
+	parser_members.set_defaults(handler = show_members)
+	parser_remove.set_defaults(handler = remove_member)
+
+if __name__ == '__main__':
+	client, args = common.init('mrlist', 'Inspect and modify Moira lists', setup_subcommands)
+	common.main()
